ff3f131425f256a12ed496374079b960f103fb55
mehrdad <hafezyhafez7@gmail.com>
2025-07-25 13:52:54 +0330

    feat: switch database backend to PostgreSQL using psycopg2-binary
    - Installed psycopg2-binary as the PostgreSQL database adapter
    - Updated DATABASES settings in settings.py to use PostgreSQL
    - Removed any references to the default SQLite setup

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
-    'markdownx',
+    'markdown',
     'debug_toolbar',
 
     'bookings',
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ticket-Reservation',           # اسم دیتابیس
+        'USER': 'postgres',         
+        'PASSWORD': 'admin',   
+        'HOST': 'localhost',     
+        'PORT': '5432',               
     }
 }
 
