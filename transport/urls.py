26c0f563bc0073d2ac9a69769bd8ccff57a0f49c
mehrdad <hafezyhafez7@gmail.com>
2025-08-01 19:02:26 +0330

    feat: register TransportViewSet with DRF router and connect API URLs

---
+++ transport/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from transport.views import TransportViewSet
+
+
+router = DefaultRouter()
+router.register(r'transport', TransportViewSet, basename='transport')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
