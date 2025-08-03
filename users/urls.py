9272a32e9c9b9f16f5698d03bd4424083ff19c88
Matin <matin.pakzad1997@gmail.com>
2025-08-03 22:10:16 +0330

    feat: add CreateStaffUserView with IsAdminUser Permission

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, CreateStaffUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('create-staff/', CreateStaffUserView.as_view(), name='create-staff'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
