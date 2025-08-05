642a0d7fa8ac2762aef66069bc5e94efd87d336a
mehrdad <hafezyhafez7@gmail.com>
2025-08-05 10:51:51 +0330

    feat(booking): add BookingSerializer with full validation logic
    - Implemented BookingSerializer with proper user assignment and price calculation
    - Added custom validation:
      • Prevent booking if transport has departed
      • Prevent overbooking beyond capacity
      • Disallow duplicate seat numbers
      • Prevent users from booking the same transport more than once
    - Set read-only fields for status and booking_date

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Booking
-
+from transport.models import Transport
+from django.utils import timezone
 
 
 
@@ -18,16 +19,30 @@ class BookingSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         transport = attrs.get('transport')
         seat_number = attrs.get('seat_number')
+        user = self.context['request'].user
+
+         # 1. زمان حرکت گذشته نباشد
+        if transport.departure_date_time <= timezone.now():
+            raise serializers.ValidationError("زمان حرکت این وسیله نقلیه گذشته است.")
+
+         # 2. ظرفیت پر نشده باشد
+        booked_count = Booking.objects.filter(transport=transport, status='active').count()
+        if booked_count >= transport.capacity:
+            raise serializers.ValidationError("ظرفیت این وسیله نقلیه پر شده است.")
+        
+
 
         if Booking.objects.filter(transport=transport, seat_number=seat_number).exists():
             raise serializers.ValidationError("این صندلی قبلاً رزرو شده است.")
+        
+
+        # 4. یک کاربر دو بار برای یک transport رزرو نکند
+        if Booking.objects.filter(transport=transport, user=user, status='active').exists():
+            raise serializers.ValidationError("شما قبلاً برای این سفر رزرو ثبت کرده‌اید.")
 
         return attrs
 
     def create(self, validated_data):
         transport = validated_data['transport']
-        price_per_seat = transport.price  # فرض بر اینکه Transport مدل قیمت داره
-        validated_data['total_price'] = price_per_seat
-
-        booking = Booking.objects.create(**validated_data)
-        return booking
+        validated_data['total_price'] = transport.price
+        return super().create(validated_data)
